1d699f3712d508e932606f8ea9d72abccba9f54b
Jose Luis <alu0100611281@ull.edu.es>
2017-06-12 15:20:07 +0100

    creando SliderInput y TextInput

--- server.R
+++ server.R
@@ -5,10 +5,12 @@
 # http://shiny.rstudio.com
 #
 library(manipulate)
-#library(shiny)
+library(shiny)
 
-#shinyServer(function(input, output){
-server <- function(input, output) {
+shinyServer(function(input, output,session){
+#server <- function(input, output) {
+  
+  #***********cargando datos******************************************************************
   
   output$contents <- renderTable({#funcion para cargar archivo
     inFile <- input$Datos
@@ -28,144 +30,287 @@ server <- function(input, output) {
     
     
    datos <- read.table(inFile$datapath,sep = " ",quote="\"")
+   t<-1
    
+   imprimir(datos)
+   #***********fin cargando datos******************************************************************
    
- 
-   #plot(datos,"h", xlab="Cpu", ylab="Tiempo", main="Eficiencia energética")
-  
-   imprimir<-datos
-   
-   
-  
-   
-   #grafica seleccion
-   
-  # output$plot1 <- renderPlot({
-  # plot(mtcars$wt, mtcars$mpg)
-   #})
-   output$info <- renderText({
-     
-     xy_range_str <- function(e) {
-       if(is.null(e)) return("NULL\n")
-       xmin<-round(e$xmin, 1)
-       xmax<-round(e$xmax, 1)
-       #ymax<-round(e$ymax, 1)
-       #ymin<-round(e$ymin, 1)
-       
-       paste0("xmin=", round(e$xmin, 1), " xmax=", round(e$xmax, 1), 
-              " ymin=", round(e$ymin, 1), " ymax=", round(e$ymax, 1))
-       matrix(datos)
-       
-     
-       i<-1
-       j<-1
-       c1<<-vector()
-       c2<<-vector()
-       z<-1
-   
-       for(i in seq_len(nrow(datos))) {
-         
-         for(j in seq_len(ncol(datos))){
-           if(j<=1){
-             if((matrix(datos[i,j])>=xmin) && (matrix(datos[i,j])<=xmax)){ #&& ((matrix(datos[i,j+1])>=ymin) && (matrix(datos[i,j+1])<=ymax))){
-               
-               c1[z]=matrix(datos[i,j])
-               c2[z]=matrix(datos[i,j+1])
-             
-               z=z+1
-                     
-             }
-             
-           }
-           
-         }
-       }
-       
-       datos2<<-data.frame(c1,c2)
-       
-       #plot(datos2,add=TRUE,"h", xlab="Cpu", ylab="Tiempo", main="Eficiencia energética")
-     }
-     
-     paste0(
-       "brush: ", xy_range_str(input$plots)
-     )
-     imprimir<-datos2
-   })
-  
-  # plot(datos2,"h", xlab="Cpu", ylab="Tiempo", main="Eficiencia energética")
+   #***********RANGOS MANUALES******************************************************************
+   value1 <- renderText({ input$captionmin })
+   value2 <- renderText({ input$captionmax })
 
-   #plot(datos,"h", xlab="Cpu", ylab="Tiempo", main="Eficiencia energética")
+   xmin<<-input$captionmin
+   xmax<<-input$captionmax
    
-   #Seleccion de Graficos
+   if(xmax!=0){ #Miramos si ha introducido un rango 
+     matrix(datos)
+
+          t=2
+          i<-1
+          j<-1
+          c1<<-vector()
+          c2<<-vector()
+          z<-1
+
+          for(i in seq_len(nrow(datos))) {
+
+            for(j in seq_len(ncol(datos))){
+              if(j<=1){
+                if((matrix(datos[i,j])>=xmin) && (matrix(datos[i,j])<=xmax)){ #&& ((matrix(datos[i,j+1])>=ymin) && (matrix(datos[i,j+1])<=ymax))){
+
+                  c1[z]=matrix(datos[i,j])
+                  c2[z]=matrix(datos[i,j+1])
+
+                  z=z+1
+
+                }
+
+              }
+
+            }
+          }
+
+          datos2<<-data.frame(c1,c2)
+     
+   imprimir(datos2)
    
+   }
+   #***********FIN RANGOS MANUALES******************************************************************
+   
+   #**********************Seleccionar rango con slider**************************************************************
+   
+   
+   MAX<<-0
+   MIN<<-0
+   i<-1
+   MIN=datos[i,]
+   for(i in seq_len(nrow(datos))){
+
+     if((datos[i,])>=MAX){
+       MAX=datos[i,]
+     }
+     if((datos[i,])<MIN){
+       MIN=datos[i,]
+     }
+   }
+  
+   
+     val <- input$ejex
+     
+     updateSliderInput(session, "ejex", value = val,
+                       min = MIN$V1, max = MAX$V1, step = 1)
+  
+   if(val[1]!=MAX$V1 && val[2]!=MIN$V1){
+   matrix(datos)
+   View(t)
+   t=2
+   i<-1
+   j<-1
+   c1<<-vector()
+   c2<<-vector()
+   z<-1
+
+   for(i in seq_len(nrow(datos))) {
+
+     for(j in seq_len(ncol(datos))){
+       if(j<=1){
+         if((matrix(datos[i,j])>=val[1]) && (matrix(datos[i,j])<=val[2])){ #&& ((matrix(datos[i,j+1])>=ymin) && (matrix(datos[i,j+1])<=ymax))){
+
+           c1[z]=matrix(datos[i,j])
+           c2[z]=matrix(datos[i,j+1])
+
+           z=z+1
+
+         }
+
+       }
+
+     }
+   }
+
+   datos2<<-data.frame(c1,c2)
+
+   imprimir(datos2)
+   
+   }
+ #****************************** fin slider****************************************************************************   
+   
+ #****************************** SELECCIONAR RANGO CON RATON***********************************************************  
+   
+  #  #grafica seleccion
+  #
+  # # output$plot1 <- renderPlot({
+  # # plot(mtcars$wt, mtcars$mpg)
+  #  #})
+  #  output$info <- renderText({
+  #
+  #    xy_range_str <- function(e) {
+  #      if(is.null(e)) return("NULL\n")
+  #      xmin<-round(e$xmin, 1)
+  #      xmax<-round(e$xmax, 1)
+  #      #ymax<-round(e$ymax, 1)
+  #      #ymin<-round(e$ymin, 1)
+  #
+  #      paste0("xmin=", round(e$xmin, 1), " xmax=", round(e$xmax, 1),
+  #             " ymin=", round(e$ymin, 1), " ymax=", round(e$ymax, 1))
+  #      matrix(datos)
+  #
+  #
+  #      i<-1
+  #      j<-1
+  #      c1<<-vector()
+  #      c2<<-vector()
+  #      z<-1
+  #
+  #      for(i in seq_len(nrow(datos))) {
+  #
+  #        for(j in seq_len(ncol(datos))){
+  #          if(j<=1){
+  #            if((matrix(datos[i,j])>=xmin) && (matrix(datos[i,j])<=xmax)){ #&& ((matrix(datos[i,j+1])>=ymin) && (matrix(datos[i,j+1])<=ymax))){
+  #
+  #              c1[z]=matrix(datos[i,j])
+  #              c2[z]=matrix(datos[i,j+1])
+  #
+  #              z=z+1
+  #
+  #            }
+  #
+  #          }
+  #
+  #        }
+  #      }
+  #
+  #      datos2<<-data.frame(c1,c2)
+  #
+  #      plot(datos2,add=TRUE,"h", xlab="Cpu", ylab="Tiempo", main="Eficiencia energética")
+  #    }
+  #
+  #    paste0(
+  #      "brush: ", xy_range_str(input$plots),resetOnNew=TRUE
+  #    )
+  #    imprimir<-datos2
+  #  })
+  #
+  # # plot(datos2,"h", xlab="Cpu", ylab="Tiempo", main="Eficiencia energética")
+  #
+  #  #plot(datos,"h", xlab="Cpu", ylab="Tiempo", main="Eficiencia energética")
+     #value1 <- renderText({ input$MIN})
+ #****************************** FIN SELECCIONAR RANGO CON RATON***********************************************************   
+     
+    
+   #**********************************Seleccion  de tipo de Graficos*******************************************************
+
    Grafi <- input$Graficos
    if(Grafi == "Histograma"){
-     hist(datos$V2,xlab="Cpu", ylab="Tiempo",main = "Histograma")
+     if(t==2){
+     hist(datos2$c2,xlab="Cpu", ylab="Tiempo",main = "Histograma")
+       }
+     # if(t==3){
+     #   hist(datos3$c4,xlab="Cpu", ylab="Tiempo",main = "Histograma")
+     #   }
+     if(t==1){
+       hist(datos$V2,xlab="Cpu", ylab="Tiempo",main = "Histograma")
+     }
    }
    if(Grafi == "Grafico de Dispersion"){
-     plot(datos,"h", xlab="Cpu", ylab="Tiempo", main="Eficiencia energética")
+     if(t==2){
+     plot(datos2, xlab="Cpu", ylab="Tiempo", main="Eficiencia energética")
+     }
+     # if(t==3){
+     #   plot(datos3, xlab="Cpu", ylab="Tiempo", main="Eficiencia energética")
+     # }
+     if(t==1)
+     {
+       plot(datos, xlab="Cpu", ylab="Tiempo", main="Eficiencia energética")
+     }
    }
    if(Grafi =="Diagrama de caja"){
-     boxplot(datos,main = "Boxplot", xlab="Cpu", ylab="Tiempo")
+     if(t==2){
+     boxplot(datos2,main = "Boxplot", xlab="Cpu", ylab="Tiempo")
      col = terrain.colors(3)
+     }
+     # if(t==3){
+     #   boxplot(datos3,main = "Boxplot", xlab="Cpu", ylab="Tiempo")
+     #   col = terrain.colors(3)
+     # }
+     if(t==1)
+     {
+       boxplot(datos,main = "Boxplot", xlab="Cpu", ylab="Tiempo")
+       col = terrain.colors(3)
+     }
    }
+   
    if(Grafi =="graficos cuantil"){
-     qqplot(datos$V1,datos$V2, col = "blue",xlab="Cpu", ylab="Tiempo", main = "Cuantiles")
-     
+     if(t==2){
+     qqplot(datos2$c1,datos2$c2, col = "blue",xlab="Cpu", ylab="Tiempo", main = "Cuantiles")
+     }
+     # if(t==3){
+     #   qqplot(datos3$c3,datos3$c4, col = "blue",xlab="Cpu", ylab="Tiempo", main = "Cuantiles")
+     # }
+     if(t==1){
+       qqplot(datos$V1,datos$V2, col = "blue",xlab="Cpu", ylab="Tiempo", main = "Cuantiles")
+     }
    }
    if(Grafi =="Grafico de Barras"){
-     barplot(datos$V1,xlab="Cpu", ylab="Tiempo", main = "Gráfico de barras")
+     if(t==2){
+     barplot(datos2$c1,xlab="Cpu", ylab="Tiempo", main = "Gráfico de barras")
+     }
+     # if(t==3){
+     #   barplot(datos3$c3,xlab="Cpu", ylab="Tiempo", main = "Gráfico de barras")
+     # }
+     if(t==1){
+       barplot(datos$V1,xlab="Cpu", ylab="Tiempo", main = "Gráfico de barras")
+     }
    }
-   
-   
 
-   
+   #**********************************Fin Seleccion de Graficos***************************************************************
+
+
    #Formulas
-    formu <- input$Formulas
-    if(formu =="Correlacion"){
-      plot(V1 ~ V2, data=datos,xlab="Cpu", ylab="Tiempo", main="Correlacion")
-      #points(cor(datos$V1,datos$V2))
-      }
-    if(formu =="Media"){
-      plot(datos$V1,datos$V2, xlab="Cpu", ylab="Tiempo", main="Media")
-      abline(h=(mean(datos$V1)),col=(mean(datos$V2)))
-      }
-    if(formu =="Mediana"){
-      plot(datos$V1,datos$V2, xlab="Cpu", ylab="Tiempo", main="Mediana")
-      abline(h=(median(datos$V1)),col=(median(datos$V2)))
-      }
-    if(formu =="Desviacion estandar"){
-      plot(datos$V1,datos$V2, xlab="Cpu", ylab="Tiempo", main="Desviacion estandar")
-      abline(h=(sd(datos$V1)),col=(sd(datos$V2)))
-      }
-    if(formu =="Varianza"){
-      plot(datos$V1,datos$V2,xlab="Cpu", ylab="Tiempo", main="Varianza")
-      abline(h=(var(datos$V1)),col=(var(datos$V2)))
-      }
-    if(formu =="Dispercion Lineal"){
-      plot(V1 ~ V2, data=datos,xlab="Cpu", ylab="Tiempo", main="Dispercion Lineal")
-      #points(cor(datos$V1,datos$V2))
-      lines(lowess(datos$V1~datos$V2),col="red")
-      #scatter.smooth(datos$V1~datos$V2)
-      #scatter.smooth(datos$V1~datos$V2, pch=16,cex=.6,xlab="Cpu", ylab="Tiempo", main="Dispercion Lineal")
-      }
-            
+    # formu <- input$Formulas
+    # if(formu =="Correlacion"){
+    #   plot(V1 ~ V2, data=datos,xlab="Cpu", ylab="Tiempo", main="Correlacion")
+    #   #points(cor(datos$V1,datos$V2))
+    #   }
+    # if(formu =="Media"){
+    #   plot(datos$V1,datos$V2, xlab="Cpu", ylab="Tiempo", main="Media")
+    #   abline(h=(mean(datos$V1)),col=(mean(datos$V2)))
+    #   }
+    # if(formu =="Mediana"){
+    #   plot(datos$V1,datos$V2, xlab="Cpu", ylab="Tiempo", main="Mediana")
+    #   abline(h=(median(datos$V1)),col=(median(datos$V2)))
+    #   }
+    # if(formu =="Desviacion estandar"){
+    #   plot(datos$V1,datos$V2, xlab="Cpu", ylab="Tiempo", main="Desviacion estandar")
+    #   abline(h=(sd(datos$V1)),col=(sd(datos$V2)))
+    #   }
+    # if(formu =="Varianza"){
+    #   plot(datos$V1,datos$V2,xlab="Cpu", ylab="Tiempo", main="Varianza")
+    #   abline(h=(var(datos$V1)),col=(var(datos$V2)))
+    #   }
+    # if(formu =="Dispercion Lineal"){
+    #   plot(V1 ~ V2, data=datos,xlab="Cpu", ylab="Tiempo", main="Dispercion Lineal")
+    #   #points(cor(datos$V1,datos$V2))
+    #   lines(lowess(datos$V1~datos$V2),col="red")
+    #   #scatter.smooth(datos$V1~datos$V2)
+    #   #scatter.smooth(datos$V1~datos$V2, pch=16,cex=.6,xlab="Cpu", ylab="Tiempo", main="Dispercion Lineal")
+    #   }
+
             #histograma
             #boxplot(datos)
             #boxplot(data.frame(datos))
             #x=rnorm(1000)
             #hist(datos$V2,xlab="Cpu", ylab="Tiempo", main="Correlacion")
-            
-            
-  
+
+
+
 
   })
-  
-  imprimir$plots3<-function(x){
+  #********************************Imprimir***************************************************
+  imprimir<-function(x){
  
-   plot(x,add=TRUE,"h", xlab="Cpu", ylab="Tiempo", main="Eficiencia energética")
+    plot(x,xlab="Cpu", ylab="Tiempo", main="Eficiencia energética")
   
-    
   }
-  
-}
+  #********************************fin Imprimir***********************************************
+})
--- ui.R
+++ ui.R
@@ -5,10 +5,10 @@
 # http://shiny.rstudio.com
 #
 library(manipulate)
-#library(shiny)
+library(shiny)
 
-#shinyUI(fluidPage(
-ui <- basicPage(
+shinyUI(fluidPage(
+#ui <- basicPage(
   # Application title
   titlePanel("Eficiencia energética en sistemas de cómputo de altas prestaciones"),
 
@@ -19,27 +19,41 @@ ui <- basicPage(
            ".csv")
                  
   ),
-  selectInput("Formulas", "Seleciones una formula:",
-              list(`Formulas` = c("Tabla principal","Media", "Mediana", "Desviacion estandar","Varianza","Correlacion","Dispercion Lineal")
-                   )
-  ),
+  # selectInput("Formulas", "Seleciones una formula:",
+  #             list(`Formulas` = c("Tabla principal","Media", "Mediana", "Desviacion estandar","Varianza","Correlacion","Dispercion Lineal")
+  #                  )
+  # ),
   
   selectInput("Graficos", "Seleciones una Tabla:",
               list(`Graficos` = c("Grafico de Dispersion","Histograma","Diagrama de caja","graficos cuantil","Grafico de Barras")
               )
   ),
   
+  textAreaInput("captionmin", "Xmin",value=MIN, width = "50px",height ="50px"),
+  textAreaInput("captionmax", "Xmax",value=MAX,  width = "50px",height ="50px"),
   
-    
+ 
+  
+  
+ sliderInput("ejex","Eje x",0,0,value=c(0,0),step=1),# dragRange = FALSE),
   
 
     # Show a plot of the generated distribution
     mainPanel(
-      
+      # tabsetPanel(      Sirve para ver el contenido del fichero una vez abierto
+      #   tabPanel("CSV",
+      #            h4("Vista del fichero CSV"),
+      #            tableOutput('contents')
+      #            )
+      # ),
       #tableOutput("contents"), 
-      plotOutput("plots",brush = "plots"),
-      verbatimTextOutput("info")
-      
+      #plotOutput("plots"),#brush = "plots"),
+     # verbatimTextOutput("info")
+      plotOutput("plots")
+      # tabPanel("Plot",plotOutput("plots"))
+     #verbatimTextOutput("value1"),
+      #verbatimTextOutput("value2")
+    
       
       
       
@@ -47,4 +61,4 @@ ui <- basicPage(
     )
   
   
-)
+))
